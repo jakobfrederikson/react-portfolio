554eaf640c5be5a2319de135d1b7967d46994503
Jakob <jakobfred1999@gmail.com>
2023-11-15 13:13:58 +1300

    Update about with new resume

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                    Hi, I'm <a href="./JakobFrederiksonResume.docx.pdf" target="_blank"><span className="text-yellow-400 hover:underline">Jakob</span></a>.
+                    Hi, I'm <a href="./JakobFrederiksonResume.pdf" target="_blank"><span className="text-yellow-400 hover:underline">Jakob</span></a>.
                     <br className="hidden lg:inline-block" /> I enjoy tinkering with stuff.
                 </h1>
                 <p className="mb-8 leading-relaxed">
